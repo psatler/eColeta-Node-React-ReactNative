f5ed960e051dad9fb9b0f274e1c47dcad98c0360
Pablo Satler <pablosatler@gmail.com>
2020-06-05 11:01:05 -0300

    chore(backend): update BASE_URL env vars to use local IP address instead of simply localhost

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -23,7 +23,7 @@ class PointsController {
     const serializedPoints = points.map(point => {
       return {
         ...point,
-        image_url: `http://192.168.15.4:3333/uploads/${point.image}`,
+        image_url: `${process.env.BASE_URL}/uploads/${point.image}`,
       }
     })
 
@@ -44,7 +44,7 @@ class PointsController {
 
     const serializedPoint ={
       ...point,
-      image_url: `http://192.168.15.4:3333/uploads/${point.image}`,
+      image_url: `${process.env.BASE_URL}/uploads/${point.image}`,
     }
 
     /**
